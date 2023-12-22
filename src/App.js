71f5f1c8a6d60754e2a9c804562855700b0893a4
MAHAK GUPTA <55080310+mahak-dev@users.noreply.github.com>
2023-12-23 04:07:03 +0530

    1stcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import NotesList from "./components/NotesList";
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import Search from "./components/Search";
 
-function App() {
+const App = () => {
+  const[notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "15/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "16/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "17/04/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my fourth note!",
+      date: "18/04/2021",
+    },
+  ]);
+  const [searchText, setSearchText] = useState('');
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search handleSearchNote={setSearchText}/>
+      <NotesList notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))} handleAddNote={addNote} handleDeleteNote = {deleteNote}/>
     </div>
-  );
-}
 
+  );
+};
 export default App;
